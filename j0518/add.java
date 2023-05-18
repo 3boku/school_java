3ba7fa1e56efee5efbbac54c37ee3ee796cb92d9
정삼복 <68502584+NANONANDFLASH@users.noreply.github.com>
2023-05-18 15:55:56 +0900

    Add files via upload

---
+++ j0518/add.java
@@ -0,0 +1,15 @@
+package j0518;
+
+public class add {
+	String sum(String s1, String s2) {
+		return s1+s2;
+	}
+	
+	int sum(int a, int b) {
+		return a+b;
+	}
+	
+	int sum(int a, int b, int c) {
+		return a+b+c;
+	}
+}
